3a38add0242a6a4b43f7325faed9f228fdbd2bf2
Dmitriy <disscovery.993@gmail.com>
2019-06-09 19:49:54 +0300

    update

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
+/* eslint-disable */
 import Vue from 'vue'
 import Router from 'vue-router'
-import Create from './views/Home.vue'
-
+import Create from './views/Create.vue'
 Vue.use(Router)
 
 export default new Router({
